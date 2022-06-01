6d7fc0d15080b38edde89e0b23a2c2738634bb7b
KamiliArsyad <arsyad.ik@gmail.com>
2022-06-01 10:54:58 +0800

    test commit

---
+++ src/ThousandSubsets.java
@@ -0,0 +1,10 @@
+public class ThousandSubsets{
+  public static int solution(int size) {
+    return 10;
+  }
+  
+  public static void main(String[] args) {
+    int result = solution(10);
+    System.out.println(result);
+  }
+}
