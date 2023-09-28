cdde05bdd93ebd18ca978814e48823a301e49625
KamiliArsyad <arsyad.ik@gmail.com>
2023-09-29 01:43:37 +0800

    add base layout

---
+++ cpp/baseCF.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <unordered_set>
+#include <cstring>
+#include <stack>
+#include <chrono>
+
+// Define macro for sync_with_stdio(false)
+#define fin std::ios::sync_with_stdio(false); std::cin
+
+typedef std::pair<int, int> ii;
+typedef std::vector<int> vi;
+typedef std::vector<short> vs;
+typedef std::vector<ii> vii;
+typedef std::vector<bool> vb;
+typedef long long ll;
+typedef std::tuple<int, ll, ll> edge;
+typedef std::pair<int, std::bitset<20>> ibits;
+const int INF = 1e9;
+const int MINUS_INF = -1e9;
+
+using namespace std;
+
+int main () {
+    int t; fin >> t;
+
+    while (t--) {
+        int n, k; fin >> n >> k;
+    }
+
+    return 0;
+}
